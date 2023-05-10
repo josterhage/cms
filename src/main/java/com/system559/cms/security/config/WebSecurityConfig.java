32bda2ffbe14adddb13dc4ca165e01e54bdb3e78
James Osterhage <josterhage@gmail.com>
2023-05-10 16:56:10 -0700

    refactored to use new AuthenticationFilter

--- src/main/java/com/system559/cms/security/config/WebSecurityConfig.java
+++ src/main/java/com/system559/cms/security/config/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.system559.cms.security.config;
 
+import com.system559.cms.security.CmsUsernamePasswordAuthenticationFilter;
 import com.system559.cms.security.service.UsernamePasswordAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,24 +15,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    private final UsernamePasswordAuthenticationProvider authenticationProvider;
+    private final CmsUsernamePasswordAuthenticationFilter authenticationFilter;
 
     @Autowired
-    public WebSecurityConfig(UsernamePasswordAuthenticationProvider authenticationProvider) {
-        this.authenticationProvider = authenticationProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(authenticationProvider);
-        return authenticationManagerBuilder.build();
+    public WebSecurityConfig(CmsUsernamePasswordAuthenticationFilter authenticationFilter) {
+        this.authenticationFilter = authenticationFilter;
     }
 
     @Bean
     public SecurityFilterChain web(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(authenticationFilter, CmsUsernamePasswordAuthenticationFilter.class)
+                .formLogin(Customizer.withDefaults())
+                .csrf().disable()
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/api/**").authenticated()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
