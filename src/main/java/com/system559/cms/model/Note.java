3e64a02ad3554dcd2372ff9ce66f9c15a5267f93
James Osterhage <josterhage@gmail.com>
2023-05-14 22:55:23 -0700

    removed multi-args constructor; cleared no-args constructor; added static factory methods

--- src/main/java/com/system559/cms/model/Note.java
+++ src/main/java/com/system559/cms/model/Note.java
@@ -1,19 +1,16 @@
 package com.system559.cms.model;
 
+import com.system559.cms.dto.NoteDto;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Note extends ManagedObject {
     private String name;
     private String text;
 
     public Note() {
-        super();
-    }
-
-    public Note(String id, List<User> managers, String name, String text) {
-        super(id, managers);
-        this.name = name;
-        this.text = text;
     }
 
     public String getName() {
@@ -30,5 +27,18 @@ public class Note extends ManagedObject {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public static Note createNote(String name, String text, List<String> managerIds) {
+        Note newNote = new Note();
+        newNote.id = UUID.randomUUID().toString();
+        newNote.name=name;
+        newNote.text=text;
+        newNote.managerIds = (managerIds != null) ? managerIds : new ArrayList<>();
+        return newNote;
+    }
+
+    public static Note createNoteFromDto(NoteDto dto) {
+        return createNote(dto.getName(),dto.getText(),dto.getManagerIds());
     }
 }
