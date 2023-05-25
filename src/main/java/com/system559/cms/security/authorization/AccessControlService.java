c4b374d0b78bb0b561dac8ceb41face5f925ba03
James Osterhage <josterhage@gmail.com>
2023-05-24 23:07:50 -0700

    accommodated AccessControlList refactor, removed overloads, ensured hasPermission(...) returns correct result when user doesn't have a specified permission or isn't on an object's ACL

---
+++ src/main/java/com/system559/cms/security/authorization/AccessControlService.java
@@ -0,0 +1,30 @@
+package com.system559.cms.security.authorization;
+
+import com.system559.cms.security.data.AccessControlList;
+import com.system559.cms.security.data.Permission;
+import com.system559.cms.security.repository.AccessControlEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.EnumSet;
+import java.util.Optional;
+
+@Service
+public class AccessControlService {
+    private final AccessControlEntryRepository repository;
+
+    @Autowired
+    public AccessControlService(AccessControlEntryRepository repository) {
+        this.repository = repository;
+    }
+    //TODO: implement meaningful exception classes
+    public boolean hasPermission(String objectId, String userId, Permission permission) {
+        AccessControlList entry = repository.findById(objectId).orElseThrow(RuntimeException::new);
+        return Optional.ofNullable(entry.getGrantedPermissions().get(userId)).orElse(EnumSet.noneOf(Permission.class)).contains(permission);
+    }
+
+    public EnumSet<Permission> getPermissions(String userId, String objectId) {
+        AccessControlList entry = repository.findById(userId).orElseThrow(RuntimeException::new);
+        return entry.getGrantedPermissions().get(objectId);
+    }
+}
