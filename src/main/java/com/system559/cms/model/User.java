be6058e3e944d1d4ad66e2c2b767da7f887fa667
James Osterhage <josterhage@gmail.com>
2023-05-27 00:18:03 -0700

    created JavaDoc; removed static factory method; removed multi-args constructor

--- src/main/java/com/system559/cms/model/User.java
+++ src/main/java/com/system559/cms/model/User.java
@@ -1,122 +1,212 @@
 package com.system559.cms.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.system559.cms.dto.UserDto;
 import java.util.List;
-import java.util.UUID;
 
+/**
+ * Entity representing a system user
+ * @author James Osterhage
+ * @version 0.0.1
+ * @since 0.0.1
+ */
 public class User implements TeamMember {
+    /**
+     * Unique identifier of this User
+     */
     private String id;
+    /**
+     * User's first name
+     */
     private String firstName;
+    /**
+     * User's last name
+     */
     private String lastName;
+    /**
+     * User's middle name
+     */
     private String middleName;
+    /**
+     * User's email address
+     */
     private String email;
+    /**
+     * User's birthdate expressed as milliseconds since the Unix Epoch UTC
+     */
+    //TODO: refactor to use a simple date
     private long birthdate;
+    /**
+     * Time of account created expressed as milliseconds since the Unix Epoch UTC
+     */
     private long accountCreated;
+    /**
+     * URL to this User's avatar
+     */
     private String avatar;
+    /**
+     * Flag for whether this profile can be viewed by any User or only Team Members
+     */
     private boolean isPublicProfile;
 
+    /**
+     * Returns an empty User object
+     */
     public User() {
     }
 
-    public User(String id, String firstName, String lastName, String middleName, String email, long birthdate, long accountCreated, String avatar, boolean isPublicProfile) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.middleName = middleName;
-        this.email = email;
-        this.birthdate = birthdate;
-        this.accountCreated = accountCreated;
-        this.avatar = avatar;
-        this.isPublicProfile = isPublicProfile;
-    }
-
+    /**
+     * Returns this User's unique identifier
+     * @return unique identifier
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Sets this User's unique identifier
+     * @param id unique identifier
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Returns this User's first name
+     * @return first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets this User's first name
+     * @param firstName first name
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Returns this User's last name
+     * @return last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets this User's last name
+     * @param lastName last name
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Returns this User's middle name
+     * @return middle name
+     */
     public String getMiddleName() {
         return middleName;
     }
 
+    /**
+     * Sets this User's middle name
+     * @param middleName middle name
+     */
     public void setMiddleName(String middleName) {
         this.middleName = middleName;
     }
 
+    /**
+     * Returns this User's email address
+     * @return email address
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets this User's email address
+     * @param email email address
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Returns this User's birthdate
+     * @return birthdate
+     */
     public long getBirthdate() {
         return birthdate;
     }
 
+    /**
+     * Sets this User's birthdate
+     * @param birthdate birthdate
+     */
     public void setBirthdate(long birthdate) {
         this.birthdate = birthdate;
     }
 
+    /**
+     * Returns the time this User account was created in milliseconds since the Unix Epoch UTC
+     * @return account creation time
+     */
     public long getAccountCreated() {
         return accountCreated;
     }
 
+    /**
+     * Sets the time this account was created
+     * @param accountCreated time created in milliseconds since the Unix Epoch UTC
+     */
+    public void setAccountCreated(Long accountCreated) {
+        this.accountCreated = accountCreated;
+    }
+
+    /**
+     * Returns a URL to this User's avatar
+     * @return URL to the avatar
+     */
     public String getAvatar() {
         return avatar;
     }
 
+    /**
+     * Sets the URL to this User's avatar
+     * @param avatar URL to the avatar
+     */
     public void setAvatar(String avatar) {
         this.avatar = avatar;
     }
 
+    /**
+     * Returns if this profile is publicly accessible
+     * @return accessibility
+     */
     public boolean isPublicProfile() {
         return isPublicProfile;
     }
 
+    /**
+     * Sets whether this profile is publicly accessible
+     * @param isPublicProfile public profile flag
+     */
     public void setPublicProfile(boolean isPublicProfile) {
         this.isPublicProfile = isPublicProfile;
     }
 
+    /**
+     * Because User extends {@link TeamMember} it has to implement {@link TeamMember#getTeamMembers()}.
+     * However, Users aren't teams, so this will always return an empty List.
+     * <br/>
+     * This method is not serialized to JSON for persistence or REST transfer
+     * @return empty list
+     */
     @JsonIgnore
     public List<TeamMember> getTeamMembers() {
         return List.of();
-    }
-
-    public static User create(String firstName, String lastName, String middleName, String email, long birthdate, String avatar, boolean isPublicProfile) {
-        User newUser = new User();
-
-        newUser.id=UUID.randomUUID().toString();
-        newUser.firstName=firstName;
-        newUser.lastName=lastName;
-        newUser.middleName=middleName;
-        newUser.email=email;
-        newUser.birthdate=birthdate;
-        newUser.accountCreated=System.currentTimeMillis();
-        newUser.avatar=avatar;
-        newUser.isPublicProfile=isPublicProfile;
-
-        return newUser;
-    }
-
-    public static User create(UserDto dto) {
-        return User.create(dto.getFirstName(),dto.getLastName(),dto.getMiddleName(),dto.getEmail(),dto.getBirthdate(),dto.getAvatar(),dto.isPublicProfile());
     }
 }
