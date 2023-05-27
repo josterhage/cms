2bd4960d7e9945fb26fcedaec12b85a61aa96f06
James Osterhage <josterhage@gmail.com>
2023-05-26 23:23:25 -0700

    added JavaDoc; removed static factory method

--- src/main/java/com/system559/cms/model/NoteBoard.java
+++ src/main/java/com/system559/cms/model/NoteBoard.java
@@ -1,34 +1,59 @@
 package com.system559.cms.model;
 
 import java.util.List;
-import java.util.UUID;
 
+/**
+ * Note Boards are containers for holding notes in a collaborative space.
+ * @author James Osterhage
+ * @version 0.0.1
+ * @since 0.0.1
+ */
 public class NoteBoard extends ManagedObject {
+    /**
+     * The name of this Note board
+     */
     private String name;
 
+    /**
+     * List of the ManagedObjects held by this board
+     */
     private List<ManagedObject> managedObjects;
 
+    /**
+     * Returns an empty NoteBoard object
+     */
     public NoteBoard() {
     }
 
+    /**
+     * Returns the name of this Note Board
+     * @return name of this NoteBoard
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of this NoteBoard
+     * @param name new name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
-    public List<ManagedObject> getObjects() {
+    /**
+     * Returns the List of this NoteBoard's contained objects
+     * @return list of contained objects
+     */
+    public List<ManagedObject> getManagedObjects() {
         return managedObjects;
     }
 
-    public static NoteBoard createNoteBoard(List<String> managerIds, String name, List<ManagedObject> managedObjects) {
-        NoteBoard newNoteBoard = new NoteBoard();
-        newNoteBoard.id = UUID.randomUUID().toString();
-        newNoteBoard.managerIds = managerIds;
-        newNoteBoard.name = name;
-        newNoteBoard.managedObjects = managedObjects;
-        return  newNoteBoard;
+    /**
+     * Sets the List of ManagedObjects
+     * @param managedObjects List of ManagedObjects
+     */
+    public void setManagedObjects(List<ManagedObject> managedObjects) {
+        this.managedObjects = managedObjects;
     }
 }
