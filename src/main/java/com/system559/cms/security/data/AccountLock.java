c0fa8e3a9a8b8b2758948b6ca3e115e7ed89632d
James Osterhage <josterhage@gmail.com>
2023-05-29 00:57:01 -0700

    added JavaDoc; renamed LOCK_TIMEOUT to LOCK_TIMEOUT_IN_MS; renamed endTime to endTimeInMillisecondsSinceEpoch;

--- src/main/java/com/system559/cms/security/data/AccountLock.java
+++ src/main/java/com/system559/cms/security/data/AccountLock.java
@@ -2,36 +2,75 @@ package com.system559.cms.security.data;
 
 import org.springframework.data.annotation.Id;
 
+/**
+ * Record of an active lock on a specified user account.
+ * @author James Osterhage
+ * @version 0.0.1
+ * @since 0.0.1
+ */
 public class AccountLock {
+    /**
+     * Unique identifier of the specified user.
+     */
     @Id
     private String userId;
-    private long endTime;
+    /**
+     * Time the lock ends in milliseconds since the Unix Epoch
+     */
+    private long endTimeInMillisecondsSinceEpoch;
 
+    /**
+     * Default lock timeout in milliseconds.
+     */
     //TODO: make LOCK_TIMEOUT configurable
-    private final static long LOCK_TIMEOUT = 60 * 60 * 1000;
+    private final static long LOCK_TIMEOUT_IN_MS = 60 * 60 * 1000;
 
+    /**
+     * Returns an empty AccountLock instance
+     */
     public AccountLock() {
     }
 
+    /**
+     * Returns a new AccountLock for the presented userId that ends {@link #LOCK_TIMEOUT_IN_MS} from instantiation.
+     * @param userID unique identifier of the locked user account
+     */
     public AccountLock(String userID) {
         this.userId = userID;
-        endTime = System.currentTimeMillis() + LOCK_TIMEOUT;
+        endTimeInMillisecondsSinceEpoch = System.currentTimeMillis() + LOCK_TIMEOUT_IN_MS;
     }
 
+    /**
+     * Returns a new AccountLock for the given userId that ends after a specified time.
+     * @param userID unique identifier of the locked user account
+     * @param endTime end time in milliseconds after the Unix Epoch
+     */
     public AccountLock(String userID, long endTime) {
         this.userId = userID;
-        this.endTime = endTime;
+        this.endTimeInMillisecondsSinceEpoch = endTime;
     }
 
-    public long getEndTime() {
-        return endTime;
+    /**
+     * Returns the lock's end time in milliseconds after the Unix Epoch
+     * @return end time in milliseconds after Unix Epoch
+     */
+    public long getEndTimeInMillisecondsSinceEpoch() {
+        return endTimeInMillisecondsSinceEpoch;
     }
 
+    /**
+     * Returns true if the lock's end time has passed.
+     * @return <b>true</b> if the end time has passed, <b>false</b> otherwise.
+     */
     public boolean isValid() {
-        return System.currentTimeMillis() < endTime;
+        return System.currentTimeMillis() < endTimeInMillisecondsSinceEpoch;
     }
 
+    /**
+     * Returns the default lock timeout in milliseconds.
+     * @return default lock timeout in milliseconds.
+     */
     public static long getLockTimeOut() {
-        return LOCK_TIMEOUT;
+        return LOCK_TIMEOUT_IN_MS;
     }
 }
