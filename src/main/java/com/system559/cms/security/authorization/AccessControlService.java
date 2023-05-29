f061653814fb655f3cba002f42c6adfa2e51ea23
James Osterhage <josterhage@gmail.com>
2023-05-29 00:42:17 -0700

    added JavaDoc; refactored permissions check methods to return either false (::hasPermission) or an empty EnumSet (::getPermissions) when the presented user does not have an access control list entry

--- src/main/java/com/system559/cms/security/authorization/AccessControlService.java
+++ src/main/java/com/system559/cms/security/authorization/AccessControlService.java
@@ -9,22 +9,51 @@ import org.springframework.stereotype.Service;
 import java.util.EnumSet;
 import java.util.Optional;
 
+/**
+ * Verifies whether a given user has the permissions to perform a requested action on a reosurce
+ * @author James Osterhage
+ * @version 0.0.1
+ * @since 0.0.1
+ */
 @Service
 public class AccessControlService {
     private final AccessControlListRepository repository;
 
+    /**
+     * Default constructor, {@link Autowired} by the framework
+     * @param repository framework-injected {@link AccessControlListRepository} instance
+     */
     @Autowired
     public AccessControlService(AccessControlListRepository repository) {
         this.repository = repository;
     }
-    //TODO: implement meaningful exception classes
+
+    /**
+     * Returns whether a user has specific permissions on an object. Returns false if the user isn't listed on the
+     * access control list for the object.
+     * @param objectId unique identifier to the object
+     * @param userId unique identifier to the user
+     * @param permission the single permission to check
+     * @return true if the user has the permission, false if not.
+     */
     public boolean hasPermission(String objectId, String userId, Permission permission) {
-        AccessControlList entry = repository.findById(objectId).orElseThrow(RuntimeException::new);
-        return Optional.ofNullable(entry.getGrantedPermissions().get(userId)).orElse(EnumSet.noneOf(Permission.class)).contains(permission);
+        AccessControlList entry = repository.
+                findById(objectId).
+                orElseThrow(() -> new AccessControlListNotFoundException(objectId));
+        return Optional.ofNullable(
+                entry.getGrantedPermissions().get(userId)).orElse(EnumSet.noneOf(Permission.class)).
+                contains(permission);
     }
 
-    public EnumSet<Permission> getPermissions(String userId, String objectId) {
-        AccessControlList entry = repository.findById(userId).orElseThrow(RuntimeException::new);
-        return entry.getGrantedPermissions().get(objectId);
+    /**
+     * Returns the permissions a user has on an object
+     * @param objectId unique identifier to the object
+     * @param userId unique identifier to the user
+     * @return EnumSet of the permissions user has on an object. Returns an empty set if the user is not in the access
+     * control list for the object.
+     */
+    public EnumSet<Permission> getPermissions(String objectId, String userId) {
+        AccessControlList entry = repository.findById(objectId).orElseThrow(() -> new AccessControlListNotFoundException(objectId));
+        return Optional.ofNullable(entry.getGrantedPermissions().get(userId)).orElse(EnumSet.noneOf(Permission.class));
     }
 }
